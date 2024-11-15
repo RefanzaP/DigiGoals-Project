c74a95f7f98fd96163c1d0cabf8c380fc52b33f5
Refanza Pradiptha <boringt6@gmail.com>
2024-11-15 08:31:15 +0700

    Add minor progress 1.0
    -Adding theme.dart (for theme)
    -Adding splash_screen.dart

---
+++ lib/theme/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color blue1 = const Color (0x19A7D7);
+Color blue2 = const Color (0x148DC2);
+Color blue3 = const Color (0x1597C9);
+
+Color yellow1 = const Color (0xFFC945);
+
+Color white1 = const Color (0xFFFFFF);
+
